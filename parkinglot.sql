36520d6f15806cb5b1b530f46f56b1b3fa005538
Rafael Goulart <rafaelxms@hotmail.com>
2025-02-03 23:12:18 -0300

    added INSERTS

--- parkinglot.sql
+++ parkinglot.sql
@@ -9,5 +9,12 @@ CREATE TABLE cars (
     parkingspace varchar(3) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET= utf8;
 
+INSERT INTO cars(brand, model, platenumber, parkingspace) VALUES
+('BMW', '320i', 'ABC0001', 'A01'),
+('BMW', 'X1', 'ABC0002', 'A02'),
+('AUDI', 'Q3', 'ABC0003', 'A03'),
+('AUDI', 'S4', 'ABC0004', 'A04'),
+('Chevrolet', 'corsa', 'DMG0286', 'A05');
+
 DESCRIBE cars;
 SELECT * FROM cars;
