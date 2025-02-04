1a04ac8ecab76fe60399dc19b1426c86f15c36e1
Rafael Goulart <rafaelxms@hotmail.com>
2025-02-03 23:13:38 -0300

    pings & listCars

--- main.go
+++ main.go
@@ -7,6 +7,7 @@ import (
 	"time"
 
 	"github.com/gin-gonic/gin"
+	"github.com/rafaeletec/parking-lot/models"
 )
 
 var now = time.Now().Local().UTC().Format("02-01-2006 15:04:05")
@@ -23,8 +24,12 @@ func main() {
 }
 
 func listCars(ctx *gin.Context) {
-	parkinglot := models.listCars()
+	parkinglot := models.ListCars()
 	if parkinglot == nil || len(parkinglot) == 0 {
+		ctx.JSON(404, gin.H{
+			"testing": "ListCars",
+			"message": "Empty database",
+		})
 		ctx.AbortWithStatus(http.StatusNotFound)
 	} else {
 		ctx.IndentedJSON(http.StatusOK, parkinglot)
@@ -32,22 +37,27 @@ func listCars(ctx *gin.Context) {
 }
 
 func pingDatabase(ctx *gin.Context) {
-	conn, err := sql.Open("sql", "")
+	conn, err := sql.Open("mysql", "root:rafael31415@/parkinglot")
 	if err != nil {
-		log.Fatalf("Can't connect to H2 Database: %s", err)
+		log.Fatalf("Can't connect to MySQL Database: %s", err)
 	}
 
 	err = conn.Ping()
 	if err != nil {
-		log.Fatalf("Can't ping to H2 Database: %s", err)
+		log.Fatalf("Can't ping to MySQL Database: %s", err)
 	}
 
-	log.Printf("H2 Database connected")
+	ctx.JSON(200, gin.H{
+		"testing": "database",
+		"message": "pong",
+	})
+	log.Printf("MySQL Database connected")
 	conn.Close()
 }
 
 func pingGin(ctx *gin.Context) {
 	ctx.JSON(200, gin.H{
+		"testing": "gin",
 		"message": "pong",
 	})
 	log.Printf("Gin is working")
