4feef3e07d7e98c398b1b573706bc60afe648406
Rafael Goulart <rafaelxms@hotmail.com>
2025-02-04 16:04:24 -0300

    added new methods

--- main.go
+++ main.go
@@ -19,6 +19,11 @@ func main() {
 	router.GET("/ping/gin", pingGin)
 
 	router.GET("/cars", listCars)
+	router.GET("/cars/:id", getCarByID)
+
+	router.POST("/cars", addCar)
+
+	router.DELETE("/cars/:id", deleteCarByID)
 
 	router.Run("localhost:8080")
 }
@@ -33,6 +38,58 @@ func listCars(ctx *gin.Context) {
 		ctx.AbortWithStatus(http.StatusNotFound)
 	} else {
 		ctx.IndentedJSON(http.StatusOK, parkinglot)
+	}
+}
+
+func addCar(ctx *gin.Context) {
+	var car models.Car
+
+	if err := ctx.BindJSON(&car); err != nil ||
+		car.BRAND == "" ||
+		car.MODEL == "" ||
+		car.PLATENUMBER == "" ||
+		car.PARKINGSPACE == "" {
+		ctx.JSON(400, gin.H{
+			"testing": "addCar",
+			"message": "Error while performing action",
+		})
+		ctx.AbortWithStatus(http.StatusBadRequest)
+	} else {
+		models.AddCar(car)
+		ctx.IndentedJSON(http.StatusCreated, car)
+	}
+}
+
+func getCarByID(ctx *gin.Context) {
+	id := ctx.Param("id")
+
+	car := models.GetCarByID(id)
+	if car == nil {
+		ctx.JSON(404, gin.H{
+			"testing": "getCarByID",
+			"message": "Car not found",
+		})
+		ctx.AbortWithStatus(http.StatusNotFound)
+	} else {
+		ctx.IndentedJSON(http.StatusOK, car)
+	}
+}
+
+func deleteCarByID(ctx *gin.Context) {
+	id := ctx.Param("id")
+
+	if car := models.GetCarByID(id); car == nil {
+		ctx.JSON(404, gin.H{
+			"testing": "deleteCarByID",
+			"message": "Car not found",
+		})
+		ctx.AbortWithStatus(http.StatusNotFound)
+	} else {
+		models.DeleteCarByID(id)
+		ctx.JSON(200, gin.H{
+			"testing": "deleteCarByID",
+			"message": "Car deleted with success",
+		})
 	}
 }
 
@@ -62,11 +119,3 @@ func pingGin(ctx *gin.Context) {
 	})
 	log.Printf("Gin is working")
 }
-
-// curl http://localhost:8080/cars \
-//     --include --header \
-//     "Content-Type: application/json" \
-//     --request "POST" --data \
-//     '{"id": "6","car_model": "corsa","car_brand": "chevrolet","time_entrance": "22-09-2024 16:05:00"}'
-
-//curl http://localhost:8080/cars --include --header "Content-Type: application/json" --request "POST" --data '{"id": "6", "car_model": "Corsa", "car_brand": "Chevrolet", "time_entrance": "22-09-2024-16:05:00"}'
--- models/cardb.go
+++ models/cardb.go
@@ -9,16 +9,18 @@ import (
 
 const dbuser = "root"
 const dbname = "parkinglot"
+const dbpass = "rafael31415"
 
 func ListCars() []Car {
-	db, err := sql.Open("mysql", "root:rafael31415@/parkinglot")
+	db, err := sql.Open("mysql", dbuser+":"+dbpass+"@/"+dbname)
 	if err != nil {
 		fmt.Println("Err ", err.Error())
 		return nil
 	}
 	defer db.Close()
 
-	results, err := db.Query("SELECT * FROM cars")
+	results, err := db.Query(
+		"SELECT * FROM cars;")
 	if err != nil {
 		fmt.Println("Err ", err.Error())
 		return nil
@@ -35,4 +37,59 @@ func ListCars() []Car {
 		parkinglot = append(parkinglot, car)
 	}
 	return parkinglot
+}
+
+func AddCar(car Car) {
+	db, err := sql.Open("mysql", dbuser+":"+dbpass+"@/"+dbname)
+	if err != nil {
+		fmt.Println("Err", err.Error())
+	}
+	defer db.Close()
+
+	add, err := db.Query(
+		"INSERT INTO cars(brand, model, platenumber, parkingspace) VALUES (?,?,?,?);",
+		car.BRAND, car.MODEL, car.PLATENUMBER, car.PARKINGSPACE)
+	if err != nil {
+		fmt.Println("Err", err.Error())
+	}
+	defer add.Close()
+}
+
+func GetCarByID(id string) *Car {
+	db, err := sql.Open("mysql", dbuser+":"+dbpass+"@/"+dbname)
+	if err != nil {
+		fmt.Println("Err", err.Error())
+	}
+	defer db.Close()
+
+	results, err := db.Query("SELECT * FROM cars WHERE id=?;", id)
+	if err != nil {
+		fmt.Println("Err", err.Error())
+		return nil
+	}
+
+	car := &Car{}
+	if results.Next() {
+		err = results.Scan(&car.ID, &car.BRAND, &car.MODEL, &car.PLATENUMBER, &car.PARKINGSPACE)
+		if err != nil {
+			return nil
+		}
+	} else {
+		return nil
+	}
+	return car
+}
+
+func DeleteCarByID(id string) {
+	db, err := sql.Open("mysql", dbuser+":"+dbpass+"@/"+dbname)
+	if err != nil {
+		fmt.Println("Err", err.Error())
+	}
+	defer db.Close()
+
+	delete, err := db.Query("DELETE FROM cars WHERE id=?;", id)
+	if err != nil {
+		fmt.Println("Err", err.Error())
+	}
+	defer delete.Close()
 }
